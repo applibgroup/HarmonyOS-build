7c77997354f1d07f2ed085097134c9e4e723fe2d
sasirekha <101694902+sasirekha111@users.noreply.github.com>
2022-04-28 12:21:53 +0530

    Update addplugin.bat

--- addplugin.bat
+++ addplugin.bat
@@ -36,10 +36,11 @@ pause >nul
 
 echo %github.repository%
 echo %GITHUB_REPOSITORY%
-echo $%GITHUB_REPOSITORY% = $%git% -replace "[/]", "_"
+SET _result=%_%GITHUB_REPOSITORY%:/=_ %
+ECHO %_result% 
 powershell -Command "(get-content build.gradle) -replace """"apply plugin: 'com.huawei.ohos.app'"""", """"$&`napply plugin: 'org.sonarqube'"""" | set-content build.gradle"
 powershell -Command "(get-content build.gradle) -replace """"classpath 'com.huawei.ohos:decctest:1.2.6.0'"""", """"$&`nclasspath 'org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.3'"""" | set-content build.gradle"
-powershell -Command "add-content build.gradle \" sonarqube {`r`n`tproperties {`r`n`t property `""sonar.projectKey"`"", ""`"%git%"`"" `r`n`t property "`""sonar.organization"`"", "`""applibgroup"`""`r`n`t property "`""sonar.host.url"`"", "`""https://sonarcloud.io/"`""`r`n`t property "`""sonar.sources"`"", "`""entry/src"`""`r`n`t property "`""sonar.java.binaries"`"", "`""entry/build"`"" `r`n`t property `""sonar.eslint.reportPaths"`"", "`""D:/a/"%GITHUB_REPOSITORY%"/"%GITHUB_REPOSITORY%"/result.json"`"" `r`n`t}`r`n}\""
+powershell -Command "add-content build.gradle \" sonarqube {`r`n`tproperties {`r`n`t property `""sonar.projectKey"`"", ""`"%_result%"`"" `r`n`t property "`""sonar.organization"`"", "`""applibgroup"`""`r`n`t property "`""sonar.host.url"`"", "`""https://sonarcloud.io/"`""`r`n`t property "`""sonar.sources"`"", "`""entry/src"`""`r`n`t property "`""sonar.java.binaries"`"", "`""entry/build"`"" `r`n`t property `""sonar.eslint.reportPaths"`"", "`""D:/a/"%GITHUB_REPOSITORY%"/"%GITHUB_REPOSITORY%"/result.json"`"" `r`n`t}`r`n}\""
 
 
 Type build.gradle
